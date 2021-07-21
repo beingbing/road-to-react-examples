da614349ec5eee6922a4a2e1b2d98f7418a19121
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-22 03:17:11 +0530

    thu 22 jul 21 | 05 | useState hook

--- hacker-stories/src/components/search.jsx
+++ hacker-stories/src/components/search.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 const Search = () => {
 
-    const handleChange = event => console.log(event.target.value);
+    const [searchTerm, setSearchTerm] = React.useState('');
+
+    const handleChange = event => setSearchTerm(event.target.value);
 
     return (
         <React.Fragment>
             <label htmlFor="search">Search: </label>
             <input id="search" type="text" onChange={handleChange} />
+            <p>Searching for <strong>{searchTerm}</strong></p>
         </React.Fragment>
     );
 }
