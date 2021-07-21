b18e8673d7141c9a91d520269c135352a6fd88f7
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-22 02:02:23 +0530

    thu 22 jul 21 | 02 | arrow functions without blocked bodies

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -2,16 +2,32 @@ import React from 'react';
 import {List} from './components/list';
 import Search from './components/search';
 
-const App = () => {
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clarke',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
 
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <Search />
-      <hr />
-      <List />
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    <h1>My Hacker Stories</h1>
+    <Search />
+    <hr />
+    <List list={list} />
+  </div>
+);
 
 export default App;
--- hacker-stories/src/components/search.jsx
+++ hacker-stories/src/components/search.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-const Search = () => {
-    return (
-        <React.Fragment>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" />
-        </React.Fragment>
-    );
-}
+const Search = () => (
+    <React.Fragment>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" />
+    </React.Fragment>
+);
 
 export default Search;
