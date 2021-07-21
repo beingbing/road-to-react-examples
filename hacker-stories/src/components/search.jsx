77bb4d35b2d3be89483ab2b38fc11514c9847df7
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-22 03:48:06 +0530

    thu 22 jul 21 | 08 | controlled component (input)

--- hacker-stories/src/components/search.jsx
+++ hacker-stories/src/components/search.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Search = ({onSearch, searchTerm}) => (
     <React.Fragment>
         <label htmlFor="search">Search: </label>
-        <input id="search" type="text" onChange={onSearch} />
+        <input id="search" type="text" onChange={onSearch} value={searchTerm} />
         <p>Searching for <strong>{searchTerm}</strong></p>
     </React.Fragment>
 );
