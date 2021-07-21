dca2eaec74850bd6f42db1aedcf27a6161353de4
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-22 02:21:54 +0530

    thu 22 jul 21 | 04 | props and local variable

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -2,32 +2,35 @@ import React from 'react';
 import {List} from './components/list';
 import Search from './components/search';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clarke',
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-]
+const App = () => {
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
-    <Search />
-    <hr />
-    <List list={list} />
-  </div>
-);
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org',
+      author: 'Dan Abramov, Andrew Clarke',
+      num_comments: 2,
+      points: 5,
+      objectID: 1
+    }
+  ];
+
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+}
 
 export default App;
