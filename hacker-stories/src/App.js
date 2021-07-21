a32a8b090fff558465e76bd1cc413743506da4fb
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-21 18:14:57 +0530

    wed 21 jul 21 | 05 | display string primitive using array

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 
-const arr = [1,2,3,4,5]
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clarke',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+]
 
 function App() {
 
   return (
     <div>
-      <h1>Hello React</h1>
-      {arr.map(ele => <h1>{ele}</h1>)}
+      <h1>My Hacker Stories</h1>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
+      <hr />
+      {list.map(ele => {
+        return (
+          <div key={ele.objectID}>
+            <span><a href={ele.url}>{ele.title}</a></span>&nbsp;
+            <span>{ele.author}</span>&nbsp;
+            <span>{ele.num_comments}</span>&nbsp;
+            <span>{ele.points}</span>&nbsp;
+          </div>
+        );
+      })}
     </div>
   );
 }
