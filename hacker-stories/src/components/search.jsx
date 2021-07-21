432346f7976847f743cfebf615b02aa4f7b860e5
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-22 02:15:38 +0530

    thu 22 jul 21 | 03 | synthetic events and event handler

--- hacker-stories/src/components/search.jsx
+++ hacker-stories/src/components/search.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const Search = () => (
-    <React.Fragment>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" />
-    </React.Fragment>
-);
+const Search = () => {
+
+    const handleChange = event => console.log(event.target.value);
+
+    return (
+        <React.Fragment>
+            <label htmlFor="search">Search: </label>
+            <input id="search" type="text" onChange={handleChange} />
+        </React.Fragment>
+    );
+}
 
 export default Search;
