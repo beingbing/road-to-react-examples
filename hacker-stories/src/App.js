e55f721082e811fd3e1d2abb369ba685364cbdc5
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-22 03:41:24 +0530

    thu 22 jul 21 | 07 | lifting state and filtering props

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -23,14 +23,18 @@ const App = () => {
     }
   ];
 
-  const handleSearch = event => console.log('event.target.value ', event.target.value);
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleSearch = event => setSearchTerm(event.target.value);
+
+  const searchedStories = stories.filter(story => story.title.toLowerCase().includes(searchTerm.toLowerCase()));
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
-      <Search onSearch={handleSearch} />
+      <Search onSearch={handleSearch} searchTerm={searchTerm} />
       <hr />
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 }
--- hacker-stories/src/components/search.jsx
+++ hacker-stories/src/components/search.jsx
@@ -1,21 +1,11 @@
 import React from 'react';
 
-const Search = ({onSearch}) => {
-
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-    const handleChange = event => {
-        setSearchTerm(event.target.value);
-        onSearch(event);
-    };
-
-    return (
-        <React.Fragment>
-            <label htmlFor="search">Search: </label>
-            <input id="search" type="text" onChange={handleChange} />
-            <p>Searching for <strong>{searchTerm}</strong></p>
-        </React.Fragment>
-    );
-}
+const Search = ({onSearch, searchTerm}) => (
+    <React.Fragment>
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text" onChange={onSearch} />
+        <p>Searching for <strong>{searchTerm}</strong></p>
+    </React.Fragment>
+);
 
 export default Search;
