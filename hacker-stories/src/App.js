37f277b0d6406d3aef470d9dd1fcfe12436faad0
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-21 18:02:10 +0530

    wed 21 jul 21 | 04:exe | display string primitive using array

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+const arr = [1,2,3,4,5]
 
 function App() {
 
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>Hello React</h1>
+      {arr.map(ele => <h1>{ele}</h1>)}
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" />
     </div>
