2f871ddfc19f6d3432f289442a6676fb7f195b2f
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-25 03:35:00 +0530

    sun 25 jul 21 | 04 | react fragements shorthand

--- hacker-stories/src/components/search.jsx
+++ hacker-stories/src/components/search.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const Search = ({onSearch, searchTerm}) => (
-    <React.Fragment>
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text" onChange={onSearch} value={searchTerm} />
-        <p>Searching for <strong>{searchTerm}</strong></p>
-    </React.Fragment>
+const Search = ({ onSearch, searchTerm }) => (
+  <>
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" onChange={onSearch} value={searchTerm} />
+    <p>
+      Searching for <strong>{searchTerm}</strong>
+    </p>
+  </>
 );
 
 export default Search;
