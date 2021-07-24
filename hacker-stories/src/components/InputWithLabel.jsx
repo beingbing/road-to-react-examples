b084816d1dc287188d299ecd1d00dd7fca5de6d6
Samar Taj Shaikh <shaikhsamartaj@gmail.com>
2021-07-25 04:46:22 +0530

    sun 25 jul 21 | 08 | focus on input field : imperative approach

--- hacker-stories/src/components/InputWithLabel.jsx
+++ hacker-stories/src/components/InputWithLabel.jsx
@@ -7,20 +7,31 @@ const InputWithLabel = ({
   value,
   type = "text",
   isFocused,
-}) => (
-  <>
-    <label htmlFor={id}>{children} </label>
-    <input
-      id={id}
-      type={type}
-      onChange={onInputChange}
-      value={value}
-      autoFocus={isFocused}
-    />
-    <p>
-      Searching for <strong>{value}</strong>
-    </p>
-  </>
-);
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children} </label>
+      <input
+        ref={inputRef}
+        id={id}
+        type={type}
+        onChange={onInputChange}
+        value={value}
+        autoFocus={isFocused}
+      />
+      <p>
+        Searching for <strong>{value}</strong>
+      </p>
+    </>
+  );
+};
 
 export default InputWithLabel;
